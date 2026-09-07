c6edce4f78987294969acd5de0fbe174a0981982
Miriam Karimi <miriamkarimi46@gmail.com>
2026-09-07 11:45:14 +0300

    chore: add geo package with feature client and geospatial utils

---
+++ packages/geo/src/index.ts
@@ -0,0 +1,9 @@
+export { GeoApiError, GeoFeatureClient } from "./features";
+export { featuresToBbox } from "./geometry";
+export type {
+  GeoFeature,
+  GeoFeatureCollection,
+  GeoFeatureProperties,
+  GeoGeometry,
+  GeoJsonGeometry,
+} from "./types";
