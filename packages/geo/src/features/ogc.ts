a6170ab7288e1823f61c35089e065e9d897f2459
Miriam Karimi <miriamkarimi46@gmail.com>
2026-09-10 13:41:52 +0300

    chore: move the geo client to the OGC maps API

---
+++ packages/geo/src/features/ogc.ts
@@ -0,0 +1,8 @@
+/**
+ * The OGC identifiers the map client matches on. These strings come from the
+ * OGC standard not from us, so they live in one place with plain names and
+ * the client reads them by name.
+ */
+export const TILESETS_VECTOR_REL = "http://www.opengis.net/def/rel/ogc/1.0/tilesets-vector";
+export const TILE_ITEM_REL = "item";
+export const MVT_MEDIA_TYPE = "application/vnd.mapbox-vector-tile";
