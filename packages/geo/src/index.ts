3484902772e51881abfdfa46dbfca69d7ddc6895
Miriam Karimi <miriamkarimi46@gmail.com>
2026-09-10 15:21:06 +0300

    chore: add the geo client write path for features

--- packages/geo/src/index.ts
+++ packages/geo/src/index.ts
@@ -1,12 +1,14 @@
 export { ApiClient } from "./features/client";
-export { GeoApiError } from "./utils/errors/geo_api";
 export type {
   BBox,
   Collection,
+  CreateFeatureInput,
   GeoApiConfig,
   GeoFeature,
   GeoFeatureProperties,
   GeoGeometry,
   OgcLink,
   TileSets,
+  UpdateFeatureInput,
 } from "./types";
+export { GeoApiError } from "./utils/errors/geo_api";
